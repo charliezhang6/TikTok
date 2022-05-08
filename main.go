7834ed578673d4f2b6d95121f144748ee06be5ac
charliezhang <charliezhang@zju.edu.cn>
2022-05-08 13:21:07 +0800

    update

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"TikTok/repository"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
 	r := gin.Default()
 
 	initRouter(r)
+	err := repository.Init()
+	if err != nil {
+		log.Fatal("数据库连接失败")
+	}
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- repository/database.go
+++ repository/database.go
@@ -9,7 +9,7 @@ var db *gorm.DB
 
 func Init() error {
 	var err error
-	dsn := "root:980926@tcp(121.40.150.207:3306)/tiktok?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(121.40.150.207:3306)/tiktok?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	return err
 }
