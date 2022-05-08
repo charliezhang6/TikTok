33f7dc6d425e2edae86dbabc0ede66f4932d4d52
charliezhang <charliezhang@zju.edu.cn>
2022-05-08 22:58:50 +0800

    update

--- redis/init.go
+++ redis/init.go
@@ -2,14 +2,14 @@ package redis
 
 import "github.com/go-redis/redis"
 
-var RedisClient *redis.Client
+var Client *redis.Client
 
 func InitRedis() error {
-	RedisClient = redis.NewClient(&redis.Options{
+	Client = redis.NewClient(&redis.Options{
 		Addr:     "121.40.150.207:6379", // 指定
 		Password: "redis",
 		DB:       0, // redis一共16个库，指定其中一个库即可
 	})
-	_, err := RedisClient.Ping().Result()
+	_, err := Client.Ping().Result()
 	return err
 }
--- redis/service.go
+++ redis/service.go
@@ -4,15 +4,15 @@ import "TikTok/util"
 
 func Set(key string, value interface{}, expireTime int64) (err error) {
 	if expireTime > 0 {
-		_, err = RedisClient.Do("SETEX", key, expireTime, value).Result()
+		_, err = Client.Do("SETEX", key, expireTime, value).Result()
 	} else {
-		_, err = RedisClient.Do("SET", key, value).Result()
+		_, err = Client.Do("SET", key, value).Result()
 	}
 	return
 }
 
 func Get(key string) (result interface{}, err error) {
-	redisVal, err := RedisClient.Do("GET", key).Result()
+	redisVal, err := Client.Do("GET", key).Result()
 	if err != nil {
 		return nil, err
 	}
