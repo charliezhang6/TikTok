850d65a4607ab9d355ff7c7a44e5cca0ee3cfdf8
charliezhang <charliezhang@zju.edu.cn>
2022-05-10 00:11:37 +0800

    Update

--- main.go
+++ main.go
@@ -1,30 +1,23 @@
 package main
 
 import (
-	"TikTok/controller"
 	"TikTok/redis"
-	"fmt"
+	"TikTok/repository"
+	"github.com/gin-gonic/gin"
 	"log"
 )
 
 func main() {
-	//r := gin.Default()
-	//
-	//initRouter(r)
-	//err := repository.Init()
-	//if err != nil {
-	//	log.Fatal("数据库连接失败")
-	//}
-	//r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-	err := redis.InitRedis()
+	r := gin.Default()
+
+	initRouter(r)
+	err := repository.Init()
+	if err != nil {
+		log.Fatal("数据库连接失败")
+	}
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err = redis.InitRedis()
 	if err != nil {
 		log.Fatal("Redis连接失败")
 	}
-	var user controller.User = controller.User{Name: "test", Id: 1111, FollowCount: 123, FollowerCount: 12345, IsFollow: true}
-	redis.Set("testtoken", user, 0)
-
-	//jsonbyte, _ := util.DefaultTranscoder.Marshal(user)
-	var newuser controller.User
-	redis.Get("testtoken", &newuser)
-	fmt.Println(newuser)
 }
