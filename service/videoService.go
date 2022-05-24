c7c3311de9177022ec3bc288af2b4d79547455e3
beihan <3418063206@qq.com>
2022-05-24 10:24:00 +0800

    finish the publishlist func but have not establelish the video table

---
+++ service/videoService.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+	"TikTok/repository"
+	"TikTok/vo"
+	"log"
+)
+
+func Getvideos(userId int64) ([]vo.Video, int) {
+	var videos []vo.Video
+	videos, err := repository.NewVideoDaoInstance().SelectById(userId)
+	if err != nil {
+		log.Println(err)
+		return nil, 1
+	}
+	if err != nil {
+		log.Println(err)
+		return nil, -1
+	}
+	return videos, 0
+}
