f4224700b89387f4f9971d5b4f9edd29b3a9eccc
lxbkkk <1725045650@qq.com>
2022-05-26 15:29:10 +0800

    publish

--- controller/publish.go
+++ controller/publish.go
@@ -51,7 +51,7 @@ func Publish(c *gin.Context) {
 		return
 	}
 
-	storePath := "D:/gotmp/"
+	storePath := "D:/VideoAndImage/"
 	filename := filepath.Base(data.Filename)
 	user := usersLoginInfo[token]
 	finalName := fmt.Sprintf("%d_%s", user.Id, filename)
