2597179646a5254aabd0bda7a2ddff9c016b81ad
charliezhang <charliezhang@zju.edu.cn>
2022-06-06 22:40:48 +0800

    update

--- repository/video.go
+++ repository/video.go
@@ -18,7 +18,7 @@ type Video struct {
 	FavoriteCount int64   `gorm:"column:favorite_count" json:"favorite_count"`    // 视频的点赞总数
 	CommentCount  int64   `gorm:"column:comment_count" json:"comment_count"`      // 视频的评论总数
 	//结构体变量isfavorite是否点赞等到点赞列表完成后再写
-	IsFavorite bool      `json:"is_favorite"`                  // true-已点赞，false-未点赞
+	IsFavorite bool      `gorm:"-" json:"is_favorite"`         // true-已点赞，false-未点赞
 	Title      string    `gorm:"column:title" json:"title"`    // 视频标题
 	DateTime   time.Time `json:"time" gorm:"column:date_time"` // 视频上传时间
 }
