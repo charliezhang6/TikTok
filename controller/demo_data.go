6364b2532add0e759801abe49a09f7094d7742d4
charliezhang <charliezhang@zju.edu.cn>
2022-06-08 14:32:21 +0800

    update

--- controller/demo_data.go
+++ controller/demo_data.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"TikTok/repository"
 	"TikTok/vo"
 )
 
@@ -17,27 +16,27 @@ var DemoVideos = []vo.Video{
 	},
 }
 
-var DemoTestVideos = []repository.Video{
-	{
-		Author:        DemoUser,
-		CommentCount:  0,
-		CoverURL:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
-		FavoriteCount: 0,
-		ID:            1,
-		IsFavorite:    false,
-		PlayURL:       "https://www.w3schools.com/html/movie.mp4",
-		Title:         "test title",
-	},
-}
-
-var DemoComments = []vo.Comment{
-	{
-		Id:         1,
-		User:       DemoUser,
-		Content:    "Test Comment",
-		CreateDate: "05-01",
-	},
-}
+//var DemoTestVideos = []repository.Video{
+//	{
+//		Author:        DemoUser,
+//		CommentCount:  0,
+//		CoverURL:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
+//		FavoriteCount: 0,
+//		ID:            1,
+//		IsFavorite:    false,
+//		PlayURL:       "https://www.w3schools.com/html/movie.mp4",
+//		Title:         "test title",
+//	},
+//}
+//
+//var DemoComments = []vo.Comment{
+//	{
+//		Id:         1,
+//		User:       DemoUser,
+//		Content:    "Test Comment",
+//		CreateDate: "05-01",
+//	},
+//}
 
 var DemoUser = vo.User{
 	Id:            1,
