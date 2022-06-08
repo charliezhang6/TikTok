7606e0c82c6f9baabac458c025aec8d5d2e1cd25
charliezhang <charliezhang@zju.edu.cn>
2022-06-08 09:36:13 +0800

    update

--- controller/user.go
+++ controller/user.go
@@ -11,19 +11,6 @@ import (
 	"strconv"
 )
 
-// usersLoginInfo use map to store user info, and key is username+password for demo
-// user data will be cleared every time the server starts
-// test data: username=zhanglei, password=douyin
-var usersLoginInfo = map[string]vo.User{
-	"zhangleidouyin": {
-		Id:            1,
-		Name:          "zhanglei",
-		FollowCount:   10,
-		FollowerCount: 5,
-		IsFollow:      true,
-	},
-}
-
 func Register(c *gin.Context) {
 	username := c.Query("username")
 	password := c.Query("password")
